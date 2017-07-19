e85ea3f5de3f453ec454048cf62fc52912466d91
Ti Technologies <titechnologies@Tis-Mac-mini-2.local>
2017-07-19 13:45:40 +0530

    registar

--- BuddyApp/RegisterViewController.swift
+++ BuddyApp/RegisterViewController.swift
@@ -7,8 +7,11 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+import GoogleSignIn
 
-class RegisterViewController: UIViewController {
+class RegisterViewController: UIViewController,GIDSignInUIDelegate {
 
     @IBOutlet weak var email_txt: UITextField!
     @IBOutlet weak var lastname_txt: UITextField!
@@ -17,6 +20,10 @@ class RegisterViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        GIDSignIn.sharedInstance().signOut()
+        // Do any additional setup after loading the view.
+        GIDSignIn.sharedInstance().uiDelegate = self
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,12 +32,89 @@ class RegisterViewController: UIViewController {
     }
     
     @IBAction func Google_register(_ sender: Any) {
+        
+        GIDSignIn.sharedInstance().signIn()
     }
     @IBAction func Facebook_register(_ sender: Any) {
+        
+        let fbLoginManager : FBSDKLoginManager = FBSDKLoginManager()
+        fbLoginManager.logIn(withReadPermissions: ["email"], from: self) { (result, error) -> Void in
+            if (error == nil){
+                let fbloginresult : FBSDKLoginManagerLoginResult = result!
+                
+                if (result?.isCancelled)! {
+                    return
+                }
+                if(fbloginresult.grantedPermissions.contains("email"))
+                {
+                    self.getFBUserData()
+                }
+            }
+        }
+        
+
     }
 
     @IBAction func next_action(_ sender: Any) {
     }
+    //MARK:Google SignIn Delegate
+    
+    func sign(inWillDispatch signIn: GIDSignIn!, error: Error!) {
+        
+    }
+    // Present a view that prompts the user to sign in with Google
+    
+    func sign(_ signIn: GIDSignIn!,
+              present viewController: UIViewController!) {
+        self.present(viewController, animated: true, completion: nil)
+    }
+    // Dismiss the "Sign in with Google" view
+    
+    func sign(_ signIn: GIDSignIn!,
+              dismiss viewController: UIViewController!) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    //completed sign In
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+        
+        
+        
+        if (error == nil)
+        {
+            let userId = user.userID                  // For client-side use only!
+            let idToken = user.authentication.idToken // Safe to send to the server
+            let name = user.profile.name
+            let email = user.profile.email
+            //let userImageURL = user.profile.imageURLWithDimension(200)
+            // ...
+            
+            
+            //print(user)
+            print(userId!)
+            print(idToken!)
+            print(name!)
+            print(email!)
+        }
+        else
+        {
+            print("\(error.localizedDescription)")
+        }
+        
+        
+    }
+    func getFBUserData(){
+        if((FBSDKAccessToken.current()) != nil){
+            FBSDKGraphRequest(graphPath: "me", parameters: ["fields": "id, name, first_name, last_name, picture.type(large), email"]).start(completionHandler: { (connection, result, error) -> Void in
+                if (error == nil){
+                    
+                    print("RESULT",result!)
+                    
+                }
+            })
+        }
+    }
+    
+
     /*
     // MARK: - Navigation
 
