d021f102fa94263ec120b87f1a9923f397545d13
Ti Technologies <titechnologies@Tis-Mac-mini-2.local>
2017-07-31 17:39:22 +0530

    design intragration22

--- BuddyApp/ForgotViewController.swift
+++ BuddyApp/ForgotViewController.swift
@@ -9,11 +9,15 @@
 import UIKit
 
 class ForgotViewController: UIViewController {
+    @IBOutlet weak var Password_txt: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        self.title = "Forgot password"
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,7 +25,53 @@ class ForgotViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func ResetPW_action(_ sender: Any) {
+        
+        if Password_txt.text!.isEmpty {
+            CommonMethods.alertView(view: self, title: ALERT_TITLE, message: "Please enter email", buttonTitle: "Ok")
+        }else
+        {
+            ForgotAPI()
+        }
+        
+        
+        
+    }
+  func ForgotAPI()
+   {
+    let parameters = [
+    "email": self.Password_txt.text!]
+    
+    let headers = [
+        "device_id": appDelegate.DeviceToken,
+        "device_imei": UIDevice.current.identifierForVendor!.uuidString,
+        "device_type": "ios",
+        ]
 
+    print("parameters",parameters)
+    CommonMethods.serverCall(APIURL: "login/forgotPassword", parameters: parameters, headers: headers , onCompletion: { (jsondata) in
+        print("FORGOT RESPONSE",jsondata)
+        
+        CommonMethods.hideProgress()
+        if let status = jsondata["status"] as? Int{
+            if status == RESPONSE_STATUS.SUCCESS{
+                
+               //  self.navigationController?.popViewController(animated: true)
+        CommonMethods.alertView(view: self, title: "SUCCESS", message: "Successfully Send password", buttonTitle: "Ok")
+                
+                
+               
+                
+            }
+            else if status == RESPONSE_STATUS.FAIL{
+                CommonMethods.alertView(view: self, title: ALERT_TITLE, message: jsondata["message"] as? String, buttonTitle: "Ok")
+            }else if status == RESPONSE_STATUS.SESSION_EXPIRED{
+                self.dismissOnSessionExpire()
+            }
+        }
+    })
+    
+    }
     /*
     // MARK: - Navigation
 
