8b9cbc63152b26002ff67e947edd6e8e57728e86
Jithesh Xavier <jitheshxavier@gmail.com>
2017-08-10 15:29:36 +0530

    random select with payment

---
+++ BuddyApp/DictionaryExtension.swift
@@ -0,0 +1,22 @@
+//
+//  DictionaryExtension.swift
+//  BuddyApp
+//
+//  Created by Jithesh Xavier on 10/08/17.
+//  Copyright © 2017 Ti Technologies. All rights reserved.
+//
+
+import Foundation
+
+extension Dictionary {
+    
+    mutating func merge(with dictionary: Dictionary) {
+        dictionary.forEach { updateValue($1, forKey: $0) }
+    }
+    
+    func merged(with dictionary: Dictionary) -> Dictionary {
+        var dict = self
+        dict.merge(with: dictionary)
+        return dict
+    }
+}
